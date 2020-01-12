9c25cbbb875eb653f25c9b8bdfe4feac50f58de6
isamuishiguro <custom@hb.tp1.jp>
2020-01-12 14:53:20 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var http=require('http');
 http.createServer(function(request,response){ 
  response.writeHead(200,{'Content-Type': 'text/html'});
- response.end('Hello World!');
+ response.end('Hello Takashi!');
 }).listen(80);
